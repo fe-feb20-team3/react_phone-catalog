52813817f99d4de1a0f12bef64399419e01a42d5
Oleksii Mykhailov <alem.coding@gmail.com>
2020-06-03 15:21:14 +0300

    Fix NotFound

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -4,11 +4,6 @@ import './NotFound.scss';
 export const NotFound = () => {
   return (
     <div className="NotFound">
-      <img
-        src="images/notFound.jpg"
-        alt="Goods not found"
-        className="NotFound__Image"
-      />
       <picture>
         <source srcSet="./images/jxr/notFound.jxr" type="image/vnd.ms-photo" />
         <source srcSet="./images/jp2/notFound.jp2" type="image/jp2" />
